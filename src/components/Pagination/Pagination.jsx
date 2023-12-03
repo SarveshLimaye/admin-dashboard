a1710d28867589c472c3d9494d88bb8250679d0f
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-12-03 14:22:12 +0530

    Improved Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -12,34 +12,51 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     }
   };
 
+  const getPageButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => handleButtonClick(i)}
+          className={`mx-1 page-button ${
+            currentPage === i ? "bg-gray-100" : "bg-white"
+          }`}
+          style={{ padding: "0.5rem 1rem" }}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
+
   return (
-    <div className="flex justify-center p-2">
+    <div className="flex justify-center p-2 items-center">
       <button
-        className="mx-2 first-page"
+        className="mx-1 first-page"
         onClick={() => handleButtonClick(1)}
         disabled={currentPage === 1}
       >
         <FaFastBackward />
       </button>
       <button
-        className="mx-2 previous-page"
+        className="mx-1 previous-page"
         onClick={() => handleButtonClick(currentPage - 1)}
         disabled={currentPage === 1}
       >
         <FaStepBackward />
       </button>
-      <span className="mx-2">
-        Page {currentPage} of {totalPages}
-      </span>
+      {getPageButtons()}
       <button
         onClick={() => handleButtonClick(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className="mx-2 next-page"
+        className="mx-1 next-page"
       >
         <FaStepForward />
       </button>
       <button
-        className="mx-2 last-page"
+        className="mx-1 last-page"
         onClick={() => handleButtonClick(totalPages)}
         disabled={currentPage === totalPages}
       >
